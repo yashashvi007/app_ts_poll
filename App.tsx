88cda237b0b56717009e26e09a68a45afb0705dd
Yashashvi Maurya <yashashvimaurya@gmail.com>
2023-06-07 16:03:40 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,45 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Home';
+import Details from './Details';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+type MyProps ={
 }
+
+type MyState = {
+}
+
+type RootStackParamList = {
+  Home : undefined , 
+  Details : {
+    id : string
+  } 
+}
+
+const Stack = createStackNavigator<RootStackParamList>()
+
+class App extends React.Component<MyProps , MyState> {
+  constructor(props : MyProps){
+    super(props);
+    
+  }
+
+  render() {
+    return (
+     <NavigationContainer>
+       <Stack.Navigator>
+         <Stack.Screen name='Home' component={Home} />
+         <Stack.Screen name='Details' component={Details} />
+       </Stack.Navigator>
+     </NavigationContainer>
+    );
+  }
+}
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
